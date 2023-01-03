2c2b9d8cf97b0abc3553fa9571251d789768e2f9
dkjefilsjl <6792kjs@naver.com>
2023-01-04 02:22:51 +0900

    Feat : 클릭시 도장 찍거나 제거하기

--- createBucket.js
+++ createBucket.js
@@ -9,15 +9,31 @@ function createBucket() {
   const newDiv = document.createElement("div");
   const value = document.getElementById("inputId").value;
   const newText = document.createTextNode(value);
-  newDiv.className =
-    count % 4 === 0
-      ? "bucket img1 center"
-      : count % 4 === 1
-      ? "bucket img1 center"
-      : count % 4 === 2
-      ? "bucket img2 center"
-      : "bucket img2 center";
+  newDiv.className = "bucket " + getimageStyle(count) + " center";
+  newDiv.id = "b" + count;
   newDiv.appendChild(newText);
   document.getElementById("box").appendChild(newDiv);
   document.getElementById("inputId").type = "hidden";
+}
+
+function getimageStyle(count) {
+  return count % 4 === 0
+    ? "img1"
+    : count % 4 === 1
+    ? "img1"
+    : count % 4 === 2
+    ? "img2"
+    : "img2";
+}
+
+function checkDone(id) {
+  const oldName = document.getElementById(id).className;
+  document.getElementById(id).className =
+    "bucket " +
+    getimageStyle(getCount(id)) +
+    (oldName.includes("done") ? " center" : " center done");
+}
+
+function getCount(id) {
+  return Number(id.slice(1));
 }
